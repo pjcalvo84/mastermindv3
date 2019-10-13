71645cb355ccd169e906009b9dd86f6b5c27ee9f
Pablo Calvo <babel.pcalvo@iberia.es>
2019-10-13 17:58:37 +0200

    start client/server

---
+++ src/mastermind/distributed/LogicImplementationServer.java
@@ -0,0 +1,43 @@
+package mastermind.distributed;
+
+import mastermind.controllers.implementation.LogicImplementation;
+import mastermind.distributed.dispatchers.AttemptsDispatcher;
+import mastermind.distributed.dispatchers.BlacksDispatcher;
+import mastermind.distributed.dispatchers.ColorsDispatcher;
+import mastermind.distributed.dispatchers.DispatcherPrototype;
+import mastermind.distributed.dispatchers.FrameType;
+import mastermind.distributed.dispatchers.LooserDispatcher;
+import mastermind.distributed.dispatchers.ProposeCombinationDispatcher;
+import mastermind.distributed.dispatchers.RedoDispatcher;
+import mastermind.distributed.dispatchers.RedoableDispatcher;
+import mastermind.distributed.dispatchers.ResumeDispatcher;
+import mastermind.distributed.dispatchers.StartDispatcher;
+import mastermind.distributed.dispatchers.StateDispatcher;
+import mastermind.distributed.dispatchers.UndoDispatcher;
+import mastermind.distributed.dispatchers.UndoableDispatcher;
+import mastermind.distributed.dispatchers.WhitesDispatcher;
+import mastermind.distributed.dispatchers.WidthDispatcher;
+import mastermind.distributed.dispatchers.WinnerDispatcher;
+
+public class LogicImplementationServer extends LogicImplementation {
+
+	public void createDispatchers(DispatcherPrototype dispatcherPrototype) {
+		dispatcherPrototype.add(FrameType.START, new StartDispatcher(this.startControllerImplementation));
+		dispatcherPrototype.add(FrameType.STATE, new StateDispatcher(this.playControllerImplementation));
+		dispatcherPrototype.add(FrameType.UNDO, new UndoDispatcher(this.playControllerImplementation));
+		dispatcherPrototype.add(FrameType.REDO, new RedoDispatcher(this.playControllerImplementation));
+		dispatcherPrototype.add(FrameType.UNDOABLE, new UndoableDispatcher(this.playControllerImplementation));
+		dispatcherPrototype.add(FrameType.REDOABLE, new RedoableDispatcher(this.playControllerImplementation));
+		dispatcherPrototype.add(FrameType.WINNER, new WinnerDispatcher(this.playControllerImplementation));
+		dispatcherPrototype.add(FrameType.LOOSER, new LooserDispatcher(this.playControllerImplementation));
+		dispatcherPrototype.add(FrameType.ATTEMPTS, new AttemptsDispatcher(this.playControllerImplementation));
+		dispatcherPrototype.add(FrameType.COLORS, new ColorsDispatcher(this.playControllerImplementation));
+		dispatcherPrototype.add(FrameType.PROPOSECOMBINATION,
+				new ProposeCombinationDispatcher(this.playControllerImplementation));
+		dispatcherPrototype.add(FrameType.WIDTH, new WidthDispatcher(this.playControllerImplementation));
+		dispatcherPrototype.add(FrameType.BLACKS, new BlacksDispatcher(this.playControllerImplementation));
+		dispatcherPrototype.add(FrameType.WHITES, new WhitesDispatcher(this.playControllerImplementation));
+		dispatcherPrototype.add(FrameType.NEW_GAME, new ResumeDispatcher(this.resumeControllerImplementation));
+	}
+
+}
---
+++ src/mastermind/distributed/dispatchers/Dispatcher.java
@@ -0,0 +1,23 @@
+package mastermind.distributed.dispatchers;
+
+import mastermind.controllers.AcceptorController;
+import mastermind.distributed.dispatchers.TCPIP;
+import santaTecla.utils.TCPIP;
+
+public abstract class Dispatcher {
+	
+	protected AcceptorController acceptorController;
+	
+	protected TCPIP tcpip;
+	
+	public Dispatcher (AcceptorController acceptorController) {
+		this.acceptorController = acceptorController;
+	}
+
+	public abstract void dispatch();
+	
+	public void associate(TCPIP tcpip) {
+		this.tcpip = tcpip;
+	}
+
+}
